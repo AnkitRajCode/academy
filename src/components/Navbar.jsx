b0418aa79a1eb499980fd15fa1c59ade5207ac6a
AnkitRajCode <rajankit432@gmail.com>
2021-09-16 13:06:05 +0530

    navbar button relsolve

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import '../css/Navbar.css'
 const Navbar = () => {
     return ( 
         <div className="nav">
-                <nav id="header" className="navbar navbar-expand-lg navbar-dark fixed-top w-100">
+                <nav id="header" className="navbar navbar-expand-lg navbar-dark fixed-top">
                 <div className="container">
                 <h1 className="logo "><a href="index.html">LTC Academy</a></h1>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,8 +17,8 @@ const Navbar = () => {
                         <li className="nav-item"><a className="nav-link scrollto" href="#faq">FAQs</a></li>
                         <li className="nav-item"><a className="nav-link scrollto" href="#contact">Contact Us</a></li>
                         <li className="nav-item"><a className="nav-link scrollto" href="#team">Our Team</a></li>
-                        <li className="nav-item" ><a className="getstarted scrollto" href="/"  target="_blank">Book Free Class</a></li>
                     </ul>
+                        <a className="getstarted scrollto" href="/"  target="_blank">Book Free Class</a>
                 </div>
             </div>
                 </nav>
